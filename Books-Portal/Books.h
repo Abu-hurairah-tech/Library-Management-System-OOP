c03416303bcd8a7dad52fee425af2a6ea8345567
Abu-hurairah-tech <hurairahnazar62@gmail.com>
2026-01-21 22:51:57 +0500

    Added Status and Copies Feature

--- Books-Portal/Books.h
+++ Books-Portal/Books.h
@@ -15,25 +15,29 @@ private:
     string title_;
     string author_;
     int ISBN_;
+    string status_;
     int copies_;
 
 public:
-    Book(int i, string t, string a, int isbn);
+    Book(int i, const string &t, const string &a, int isbn);
     void displaydetails() const;
     int getID() const;
     string getTitle() const;
     string getAuthor() const;
     int getISBN() const;
     void setID(int i);
-    void setTitle(string t);
-    void setAuthor(string a);
+    void setTitle(const string &t);
+    void setAuthor(const string &a);
     void setISBN(int isbn);
-    bool operator==(int id);
-    bool operator==(int isbn);
-    bool operator==(string t);
-    bool operator==(string a);
-    string toCSV();
-    void writeToFile(ofstream &outFile);
+    string getStatus() const;
+    void setStatus(const string &status);
+    bool operator==(int id) const;
+    string toCSV() const;
+    void writeToFile(ofstream &outFile) const;
+    int getCopies() const;
+    void setCopies(int c);
+    void incrementCopies();
+    void decrementCopies();
     ~Book();
 };
 
